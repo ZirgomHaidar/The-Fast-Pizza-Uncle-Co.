75bab4bf386654a055a8b2c1a1730cd0e7ade96a
ZirgomHaidar <zirgomhaidar17@gmail.com>
2024-08-05 15:56:39 +0530

    Add and Configure react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
 import { useState } from "react"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Home from "./ui/Home"
+import Menu from "./features/menu/Menu"
+import Cart from "./features/cart/Cart"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/menu",
+    element: <Menu />,
+  },
+  {
+    path: "/cart",
+    element: <Cart />,
+  },
+])
 
 function App() {
-  return <div></div>
+  return <RouterProvider router={router} />
 }
 
 export default App
