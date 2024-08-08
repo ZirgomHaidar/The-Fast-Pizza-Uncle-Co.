a9fa3de6a5198c869f6d09a61286e94f640758d6
ZirgomHaidar <zirgomhaidar17@gmail.com>
2024-08-08 16:47:47 +0530

    Use a Grid to all item in the menu

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -4,7 +4,7 @@ import MenuItem from "./MenuItem"
 function Menu() {
   const menu = useLoaderData()
   return (
-    <ul>
+    <ul className="grid grid-cols-5 gap-4 px-32 py-10">
       {menu.map((pizza) => (
         <MenuItem pizza={pizza} key={pizza.id} />
       ))}
