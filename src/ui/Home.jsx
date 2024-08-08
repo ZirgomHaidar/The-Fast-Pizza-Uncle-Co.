44b360d915e0a97cb140f81758a5c688d3aafce0
ZirgomHaidar <zirgomhaidar17@gmail.com>
2024-08-08 16:56:34 +0530

    Upate the Home Page layout and add a flag to show button if username is entered or not

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,13 +1,30 @@
+import CreateUser from "../features/user/CreateUser"
+import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
+
 function Home() {
+  const username = useSelector((state) => state.user.username)
   return (
-    <div>
-      <h1>
+    <div className="flex flex-col items-center justify-center p-32">
+      <h1 className="mb-5 text-center text-5xl font-bold tracking-wider">
         The best pizza.
         <br />
-        Straight out of the oven, straight to you.
+        <span className="text-amber-400">
+          Straight out of the oven, straight to you.
+        </span>
       </h1>
+      {username === "" ? (
+        <CreateUser />
+      ) : (
+        <Link
+          to={"/menu"}
+          className="mt-5 rounded-full bg-yellow-500 p-5 font-bold uppercase"
+        >
+          Continue Ordering, {username}
+        </Link>
+      )}
     </div>
-  );
+  )
 }
 
-export default Home;
+export default Home
